1428f9b768e022d1014856fb71fefd0e697c7d0b
goldentoaste <rayg2375@gmail.com>
2023-05-21 13:34:35 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,25 @@
 <script>
     import BiasChecker from "./BiasChecker.svelte";
-import CharNamesGen from "./CharNamesGen.svelte";
+    import CharNamesGen from "./CharNamesGen.svelte";
     import CustomPrompt from "./CustomPrompt.svelte";
-
 </script>
 
+<CustomPrompt />
+<div style="width:auto; height:3px; background:blue; margin:3rem;" />
 
-<CustomPrompt/>
-<div style="width:auto; height:3px; background:blue; margin:3rem;"/>
+<CharNamesGen />
 
+<div style="width:auto; height:3px; background:blue; margin:3rem;" />
 
-<CharNamesGen></CharNamesGen>
-
-
-<div style="width:auto; height:3px; background:blue; margin:3rem;"/>
-
-<BiasChecker></BiasChecker>
-
-
+<BiasChecker />
 
 ---
 
-
 <p>
+   Now, I need your help to transfer this money out of the country
+    so that we can share it equally. If you agree to assist me, I will provide
+    you with all the necessary documents and information required to
+    authenticate your involvement as a beneficiary. Once the funds are
+    successfully transferred into your account, you will be entitled to 40% of
+    the total sum, which amounts to an astonishing $10,000,000!
 </p>
